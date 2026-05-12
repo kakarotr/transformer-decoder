413d472f521779aa7125a28e193d75b650e3fb36
kakarotr <18250331338@139.com>
2026-05-12 17:02:08 +0800

    u

--- data/hub/upload.py
+++ data/hub/upload.py
@@ -4,8 +4,8 @@ api = HubApi()
 api.login("ms-e2eb8794-9a83-4f6c-9fac-5ee4bba96891")
 
 api.upload_folder(
-    repo_id="kakarotter/Gllama-Dataset",
-    path_in_repo="pretraining/tokenize",
-    folder_path="F:/transformer-decoder/pretraining/tokenize",
+    repo_id="kakarotter/Calix-Dataset",
+    path_in_repo="pretraining/tokenize_corpus",
+    folder_path="F:/transformer-decoder/pretraining/tokenize_corpus",
     repo_type="dataset",
 )
